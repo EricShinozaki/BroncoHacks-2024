8418801c3b09ffb356bf1d60356d10d21213fea9
think-v2 <jaceareff@gmail.com>
2024-02-02 18:54:39 -0800

    Update content.js

--- security-extensions/content.js
+++ security-extensions/content.js
@@ -37,37 +37,3 @@ window.addEventListener('hashchange', function(){
 function scanButtonClicked() {
     return null;
 }
-
-// below is google's way of modifying a webpage, study and apply
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (sender) {
-  const text = sender.textContent;
-  /**
-   * Regular expression to find all "words" in a string.
-   *
-   * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-   * regular expression character class "\w" to match against "word characters" because it only
-   * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-   * *are not* a whitespace characters. See the below link for more information.
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-   */
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement('p');
-  // Use the same styling as the publish information in an sender's header
-  badge.classList.add('color-secondary-text', 'type--caption');
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = sender.querySelector('h1');
-  // Support for sender docs with date
-  const date = sender.querySelector('time')?.parentNode;
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-  (date ?? heading).insertAdjacentElement('afterend', badge);
-}
