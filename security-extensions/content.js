0c2eaf8032fdeec776ea5b13b86a3ff7ea979f32
think-v2 <jaceareff@gmail.com>
2024-02-02 18:53:17 -0800

    Update content.js

--- security-extensions/content.js
+++ security-extensions/content.js
@@ -1,16 +1,44 @@
 console.log("INJECTED INTO WEBPAGE");
 
-const sender = document.getElementsByClassName('gD');
-const emailBody = document.getElementsByClassName('a3s aiL ');
+//check for a change in URL
+window.addEventListener('hashchange', function(){
+    //make sure user is not in the inbox
+    if(document.URL.length>40) {
+        //grab HTML collections
+        const senderHTML = document.getElementsByClassName('gD');
+        const emailBodyHTML = document.getElementsByClassName('a3s aiL ');
+        const emailSubjectHTML = document.getElementsByClassName('hP');
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    console.log("listener called");
-    console.log(sender);
-    console.log(emailBody);
-    console.log(sender.length);
+        //name
+        var name = senderHTML[0].innerText;
+        console.log(name);
+        //email
+        var unparsedEmail = senderHTML[0].outerHTML;
+        var email = unparsedEmail.substring(7+unparsedEmail.indexOf('email="'),unparsedEmail.indexOf('"',7+unparsedEmail.indexOf('email="')));
+        //console.log(unparsedEmail);
+        console.log(email);
+        //subject
+        var subject = emailSubjectHTML[0].outerText;
+        console.log(subject);
+        //body
+        var body = emailBodyHTML[0].innerText;
+        console.log(body);
 
+        
+        let scanButton = document.createElement("button");
+        scanButton.className = "scanButton";
+        emailSubjectHTML[0].after(scanButton);
+        scanButton.outerHTML = "<button onclick='scanButtonClicked()'>Scan</button>";
+
+        
+    }
 });
+
+function scanButtonClicked() {
+    return null;
+}
+
+// below is google's way of modifying a webpage, study and apply
 
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (sender) {
